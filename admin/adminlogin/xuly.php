6fb6b3102f0698f1031273222566585681a577e7
Hieu <45428745+hieukut456@users.noreply.github.com>
2019-05-07 10:38:53 +0700

    Add files via upload

--- admin/adminlogin/xuly.php
+++ admin/adminlogin/xuly.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require("../../include/dbconnect.php");
 if(isset($_POST["action"])){
 	$ad_id = $_POST["ad_id"];
@@ -7,6 +8,13 @@ if(isset($_POST["action"])){
 	$result = mysqli_query($con,$sql);
 	if(mysqli_num_rows($result)>0)
 	{
+		while( $row = mysqli_fetch_assoc($result)){
+			if($row['id_role'] ==1)
+				$_SESSION['ad_role']='admin';
+			else $_SESSION['ad_role']='manager';
+			$_SESSION['ad_name']=$row['firstname'];
+			$_SESSION['ad_user']=$row['username'];
+		}
 		echo "yes";
 	}
 	else{
